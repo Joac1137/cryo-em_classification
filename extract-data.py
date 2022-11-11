8f79bb9d19ddce4df4b536f9a60c34e747b08d25
Jonathan Søholm-Boesen <jonasobo@gmail.com>
2022-11-11 17:56:24 +0100

    Created extract data script

---
+++ extract-data.py
@@ -0,0 +1,12 @@
+import os
+from pathlib import Path
+
+p = Path(os.getcwd()) / 'output'
+
+for file in p.iterdir():
+    if file.suffix == '.csv':
+        os.rename(str(file), os.path.join(
+            os.getcwd(), 'data/label_annotation', str(file.name)))
+    else:
+        os.rename(str(file), os.path.join(
+            os.getcwd(), 'data/raw_data', str(file.name)))
